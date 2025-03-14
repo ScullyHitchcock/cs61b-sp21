bc9aa8f59bcfe865612af5b214932b6991d5fa79
SH <tennycst1992@gmail.com>
2025-03-14 13:14:50 +0800

    Finished lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -12,8 +12,6 @@ import static capers.Utils.*;
  * .capers/ -- top level folder for all persistent data in your lab12 folder
  *    - dogs/ -- folder containing all of the persistent data for dogs
  *    - story -- file containing the current story
- *
- * TODO: change the above structure if you do something different.
  */
 public class CapersRepository {
     /** Current Working Directory. */
@@ -32,7 +30,8 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-
+        if (!CAPERS_FOLDER.exists()) CAPERS_FOLDER.mkdir();
+        if (!Dog.DOG_FOLDER.exists()) Dog.DOG_FOLDER.mkdir();
     }
 
     /**
@@ -41,17 +40,22 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        CAPERS_FOLDER.mkdir();
+
+        /* 创建 story.txt 文件 */
         File story = Utils.join(CAPERS_FOLDER, "story.txt");
         if (!story.exists()) {
             try {
                 story.createNewFile();
             } catch (IOException e) {
                 System.out.println("创建文件时发生错误：" + e.getMessage());
+                return;
             }
         }
-        Utils.writeContents(story, text, "\n");
-        String fullTest = Utils.readContentsAsString(story);
+
+        /* 读取原内容 oringinalText ，合并新内容 text 为 fullText ，再次写入文件并打印内容。*/
+        String oringinalText = Utils.readContentsAsString(story);
+        String fullTest = oringinalText + text + "\n";
+        Utils.writeContents(story, fullTest);
         System.out.println(fullTest);
     }
 
@@ -61,9 +65,10 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog dog = new Dog(name, breed, age);
+        dog.saveDog();
+        System.out.println(dog);
     }
-
     /**
      * Advances a dog's age persistently and prints out a celebratory message.
      * Also prints out the dog's information using toString().
@@ -71,6 +76,8 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog dog = Dog.fromFile(name);
+        dog.haveBirthday();
+        dog.saveDog();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,13 +1,14 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
  * @author CST
 */
-public class Dog { // TODO
+public class Dog implements Serializable {
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs");
@@ -38,8 +39,8 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File fileName = Utils.join(DOG_FOLDER, name);
+        return Utils.readObject(fileName, Dog.class);
     }
 
     /**
@@ -55,7 +56,13 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        try {
+            dogFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("创建文件时发生错误：" + e.getMessage());
+        }
+        Utils.writeObject(dogFile, this);
     }
 
     @Override
